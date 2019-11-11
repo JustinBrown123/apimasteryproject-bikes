7d09911a63ba391ee07bd6860a7fce52148855cc
Justin Brown <j-brown123@live.com>
2019-11-11 09:28:51 -0500

    created models

---
+++ backend/apimasteryproject-bikes/apimasteryproject-bikes/Models/ManufacturerStyle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apimasteryproject_bikes.Models
+{
+    public class ManufacturerStyle
+    {
+        public int StyleID { get; set; }
+        public virtual Style Styles { get; set; }
+
+        public int ManufacturerID { get; set; }
+        public virtual Manufacturer Manufacturers { get; set; }
+    }
+}
---
+++ backend/apimasteryproject-bikes/apimasteryproject-bikes/Repositories/ManufacturerRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apimasteryproject_bikes.Repositories
+{
+    public class ManufacturerRepository
+    {
+    }
+}
