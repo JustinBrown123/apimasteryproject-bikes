c4fcc7edc80abc606b844f8b49623c73cc0de713
Justin Brown <j-brown123@live.com>
2019-11-14 15:22:38 -0500

    restarted front end

---
+++ frontend/src/js/component/header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return `
+    <nav>
+    <ul class="nav__header">
+        <li class="nav__home">Home</li>
+        <li class="nav__manufacturers">Manufacturers</li>
+        <li class="nav__bicycles">Bicycles</li>
+     </ul>
+    </nav>
+    `;
+}
