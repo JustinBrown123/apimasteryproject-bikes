6656528b30afbccdefd04dd50aa6d41ea7249d47
Justin Brown <j-brown123@live.com>
2019-11-14 16:43:28 -0500

    Working on viewing manufacturers

--- frontend/src/js/component/manufacturers.js
+++ frontend/src/js/component/manufacturers.js
@@ -5,13 +5,13 @@ export default function Manufacturer(manufacturer)
             .map(manufacturer => {
                 return `
                 <div class="manufacturers"
-                
-                
-                
-                
+                    <img class="manufacturer__image" src=${manufacturer.logo}></img>
+                    <input class="manufacturer__id" type="hidden" value="${manufacturer.id}">                    
+                </div>
                 `
-            }  
+            }) 
 
         }
 
     `
+    
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,6 +1,7 @@
 import Header from "./component/header";
 import Footer from "./component/footer";
 import Home from "./component/home";
+import apiActions from "./API/apiActions";
 
 export default () =>
 {
@@ -24,4 +25,13 @@ function footer(){
 function home(){
     const app = document.querySelector('#app')
     app.innerHTML = Home();
+}
+function navManufacturers(){
+    const manufacturersButton= document.querySelector(".nav__manufacturers")
+    const app = document.querySelector("#app")
+    manufacturersButton.addEventListener("click", function(){
+        apiActions.getRequest("http://localhost:44312/api/manufacturer", manufacturers =>{
+            document.querySelector("#app").innerHTML = Manufacturers(manufacturer)
+        })
+    })
 }
