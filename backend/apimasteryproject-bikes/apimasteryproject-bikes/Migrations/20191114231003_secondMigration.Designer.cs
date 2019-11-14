5ad6860f1d29eeac18a91f66a0bdc1af6775a4b2
Justin Brown <j-brown123@live.com>
2019-11-14 18:11:25 -0500

    added database

--- backend/apimasteryproject-bikes/apimasteryproject-bikes/Migrations/20191114231003_secondMigration.Designer.cs
+++ backend/apimasteryproject-bikes/apimasteryproject-bikes/Migrations/20191114231003_secondMigration.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using apimasteryproject_bikes.Context;
 
 namespace apimasteryproject_bikes.Migrations
 {
     [DbContext(typeof(BikeContext))]
-    partial class BikeContextModelSnapshot : ModelSnapshot
+    [Migration("20191114231003_secondMigration")]
+    partial class secondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
