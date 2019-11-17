9c166c4d933a1f7a8ae70691810024873b58e151
Bryan Kautzman <bdkautz@icloud.com>
2019-11-17 15:55:39 -0500

    css work

--- frontend/src/js/component/header.js
+++ frontend/src/js/component/header.js
@@ -1,11 +1,19 @@
 export default function Header() {
     return `
     <nav>
-    <ul class="nav__header">
-        <li class="nav__home">Home</li>
-        <li class="nav__manufacturers">Manufacturers</li>
-        <li class="nav__bicycles">Bicycles</li>
-     </ul>
+        <div class="logo">
+        <h4>J&B Bikes</h4>
+        </div>
+        <div>
+            <ul class="nav__header">
+            
+                <li id="nav__home">Home</li>
+                <li id="nav__manufacturers">Manufacturers</li>
+                <li id="nav__bicycles">Bicycles</li>
+                <li id="nav__contact">Contact Us</li>
+            </ul>  
+        </div>      
+        
     </nav>
-    `;
+    `
 }
